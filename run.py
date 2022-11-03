48f5c2412f782cfe292d6f48775c11f1143be34e
OliviaButters <111284147+OliviaButters@users.noreply.github.com>
2022-11-03 10:38:24 +0000

    Update run.py

--- run.py
+++ run.py
@@ -180,4 +180,4 @@ all_builds = joined_build.to_file(os.path.join(outputs_path,'all_buildings.shp')
 # final step - delete the log file if requested by user
 if save_logfile is False:
     # delete log file dir
-    remove(join(data_path, output_dir, log_file_name))
+    remove(join(data_path, outputs_path, log_file_name))
